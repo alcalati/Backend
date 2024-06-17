ab6b6fd3aafe039a491d419ac72d367727af77ad
alcalati <alcaat@edem.es>
2024-06-17 19:34:51 +0200

    deleted comments

--- database.js
+++ database.js
@@ -1,10 +1,5 @@
 import mongoose from 'mongoose';
-// import * as dotenv from 'dotenv';
 
-// dotenv.config();
-
-// eslint-disable-next-line no-undef
-// const { MONGO_URL, MONGO_DB_NAME, } = process.env;
 const MONGO_URL = 'mongodb+srv://acalvoatienza:XEfSY-7e5uq38Ch@sneakers.pzv3v6x.mongodb.net/';
 const MONGO_DB_NAME = 'backendDB';
 
