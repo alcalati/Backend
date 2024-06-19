9cc3e285f704fcce144720c16cbe8d2a43881c70
alba-garcia09 <albagarlaz@gmail.com>
2024-06-19 20:46:58 +0200

    route getbypricerange

--- src/api/clothes/clothes.controller.js
+++ src/api/clothes/clothes.controller.js
@@ -2,8 +2,8 @@ import * as clothesService from './clothes.service.js';
 
 async function getByPriceRange(req, res) {
   const { query, } = req;
-  console.log('query', query);
-  if (!query.min || !query.max || !query) {
+
+  if (!query.min && !query.max) {
     res.status(400);
     res.json({ error: 'The properties of /byPriceRange route query are min or max', });
     return;
@@ -12,7 +12,6 @@ async function getByPriceRange(req, res) {
   const filteredClothesByPriceRange = await clothesService.getByPriceRange({ query, });
   res.json(filteredClothesByPriceRange);
 }
-
 
 export {
   getByPriceRange
--- src/api/clothes/clothes.repository.js
+++ src/api/clothes/clothes.repository.js
@@ -3,8 +3,8 @@ import clothesModel from './clothes.model.js';
 function getByPriceRange({ query, }) {
   const price = { $gte: 0, };
 
-  query.min && (price.$gte = parseInt(query.min));
-  query.max && (price.$lte = parseInt(query.max));
+  query.min && (price.$gte = parseFloat(query.min));
+  query.max && (price.$lte = parseFloat(query.max));
 
   const filteredByPriceRangeClothes = clothesModel.find({ price, }).lean();
   return filteredByPriceRangeClothes;
