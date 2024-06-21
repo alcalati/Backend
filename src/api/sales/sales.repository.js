f8478de79bb9c7a8a216b3b832903d9ede5c272d
alcalati <alcaat@edem.es>
2024-06-21 20:02:05 +0200

    removed logs

--- src/api/sales/sales.repository.js
+++ src/api/sales/sales.repository.js
@@ -1,8 +1,6 @@
 import salesModel from './sales.model.js';
 
 function refund({ id, price, stock, }) {
-  console.log('Sales price: ' + price);
-  console.log('Sales stock: ' +stock);
   const refundClothes = salesModel.create({idClothes : id, price : price , stock : stock , type: 'refund', });
   return refundClothes;
 }
