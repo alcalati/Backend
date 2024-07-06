55a8cac60f9ac0a6c7890dac8cafd5fdfe86ed79
paulotorrente <paulotorrentebr@gmail.com>
2024-07-06 22:21:17 +0200

    Final

--- src/api/sales/sales.repository.js
+++ src/api/sales/sales.repository.js
@@ -5,6 +5,20 @@ function refund({ id, price, stock, }) {
   return refundClothes;
 }
 
+async function updateSale({ id, updateData, }) {
+  const { stock, price, ...allowedUpdates } = updateData;
+
+  try {
+    const updatedSale = await salesModel.findByIdAndUpdate(id, allowedUpdates, { new: true, });
+
+    return updatedSale;
+  } catch (error) {
+    console.error(`Error updating sale: ${error.message}`);
+    throw new Error('Error updating sale');
+  }
+}
+
+
 export {
   refund
 };
