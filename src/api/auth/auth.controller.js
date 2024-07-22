fbc118edc9f82488a0124253b05a9ca7efac007c
alba-garcia09 <albagarlaz@gmail.com>
2024-07-22 18:47:56 +0200

    change register with restrictions

--- src/api/auth/auth.controller.js
+++ src/api/auth/auth.controller.js
@@ -17,9 +17,18 @@ async function login(req, res) {
 
 async function register(req, res) {
   const newUser = req.body;
-  if (!newUser.email || !newUser.password) {
+
+  if (!newUser.email || !newUser.password || !newUser.role || !newUser.cash) {
+    let message = 'The following properties are mandatory: ';
+    const emptyProps = [];
+    !newUser.email && emptyProps.push('Email');
+    !newUser.password && emptyProps.push('Password');
+    !newUser.role && emptyProps.push('Role');
+    !newUser.cash && emptyProps.push('Cash');
+    message += emptyProps.join(', ');
+
     res.status(400);
-    res.json({ error: 'ERROR: The email and password params are required', });
+    res.json({ error: message, });
     return;
   }
 
